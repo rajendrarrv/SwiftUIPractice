d55ad0722c1b9077d1c2cb2e59523c281e6ff835
Rajendra <rajendrarajaramv@gmail.com>
2022-12-09 13:50:35 +0530

    First Commit

---
+++ SwiftUIPractice/SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Apple on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
